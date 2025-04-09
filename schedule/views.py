c11de241304936c723aa5d95704cc722c796c78b
nityawastaken <ruparelnitya@gmail.com>
2025-04-09 16:20:44 +0530

    added list of views to be made

--- schedule/views.py
+++ schedule/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# fetch a class's titmetable
+# fetch a teacher's timetable
+# fetch a room's timetable
+# fetch which all subjects a teacher teaches
+# fetch which all teachers teach a class
+# fetch which all teachers teach a subject
